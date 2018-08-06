75db572866cbb125c1b3b2d37fb898dc200f851c
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-08-06 22:33:53 +0700

    Add comment

--- RabinKarp.cpp
+++ RabinKarp.cpp
@@ -13,13 +13,19 @@
  * @note   This implementation will count number of pattern string in target string
  * @param  target: given string that may contain pattern string
  * @param  pattern: string need to be find in target string
- * @retval Position of match pattern string in target string
+ * @retval Position of match pattern string in target string,
+ *         if pattern can't be found in target, return -1
  */
 
 int RabinKarp(const std::string &target, const std::string &pattern)
 {
+    /** 
+     * cnst: Random cnst number
+     * mod: Modulo
+     */
     const unsigned cnst = 257, mod = 1e9 + 7;
 
+    /* Normal hash */
     auto hash = [](const std::string &str) -> unsigned {
         long long sum = 0;
         for (auto &iter : str)
@@ -30,6 +36,7 @@ int RabinKarp(const std::string &target, const std::string &pattern)
         return sum;
     };
 
+    /* Binary exponentiation */
     auto bin_pow = [](long long a, long long b) -> long long {
         long long res = 1;
         while (b != 0)
@@ -46,32 +53,36 @@ int RabinKarp(const std::string &target, const std::string &pattern)
         return res;
     };
 
+    // Avoid overflow
     long long
-        patternSize = pattern.size(),
         targetSize = target.size(),
+        patternSize = pattern.size(),
         power = bin_pow(cnst, patternSize),
         patternHash = hash(pattern),
         rollingHash = 0;
 
+    /* Rolling Hash */
     for (long long i = 0; i < targetSize; ++i)
     {
-        // Add last character
+        /* Add last character */
         rollingHash = rollingHash * cnst + target[i];
         rollingHash %= mod;
         if (i >= patternSize)
         {
-            // Sub first character
+            /* Sub first character */
             rollingHash -= power * target[i - patternSize];
             // Case: Negative
             while (rollingHash < 0)
                 rollingHash += mod;
         }
 
-        // Compare hash
+        /* Compare hash */
         if (i >= patternSize - 1 && patternHash == rollingHash)
+            /* Return: Success */
             return (i - patternSize + 1);
     }
 
+    /* Return: Not Found */
     return -1;
 }
 
